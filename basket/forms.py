d8980f6c0f09ddf5633ea8758df990c466f1122a
gabriel <holaestoybn@gmail.com>
2018-05-29 00:14:42 -0400

    CRUD

--- basket/forms.py
+++ basket/forms.py
@@ -1,9 +1,46 @@
 from django.forms import ModelForm
-from basket.models import Player
+from basket.models import *
 
 
 class PlayerForm(ModelForm):
+    
     class Meta:
         model = Player
         fields = ['rut', 'dv', 'name', 'nickname', 'birthday', 'age', 'email', 'height', 'weight', 'picture', 'position', 'team']
 
+class CoachForm(ModelForm):
+    
+    class Meta:
+        model = Coach
+        fields = ['name', 'age', 'email', 'nickname', 'rut', 'dv']
+        
+class TeamForm(ModelForm):
+    
+    class Meta:
+        model = Team
+        fields = ['name', 'logo', 'description']
+        
+class EditPlayer(ModelForm):
+    
+    def init(self, args, kwargs):
+        super(EditPlayer, self).init(args, kwargs)
+        self.fields['picture'].required = False
+
+    class Meta:
+        model = Player
+        fields = ['rut', 'dv', 'name', 'nickname', 'birthday', 'age', 'email', 'height', 'weight', 'picture', 'position', 'team']
+        
+class EditTeam(ModelForm):
+    def init(self, args, kwargs):
+        super(EditTeam, self).init(args, kwargs)
+        self.fields['logo'].required = False
+
+    class Meta:
+        model = Team
+        fields = ['name','description','logo','code']
+        
+class EditCoach(ModelForm):
+
+    class Meta:
+        model = Coach
+        fields = ['name','age', 'email','nickname','rut','dv']
--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from basket.models import Player
-from basket.forms import PlayerForm
+from basket.models import *
+from basket.forms import *
 from django.shortcuts import redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 @login_required(login_url='/auth/login')
@@ -28,7 +30,7 @@ def index(request):
     return render(request, template_name, data)
 
 
-def add(request):
+def add_player(request):
     data = {}
     if request.method == "POST":
         data['form'] = PlayerForm(request.POST, request.FILES)
@@ -41,20 +43,90 @@ def add(request):
 
     else:
         data['form'] = PlayerForm()
-
-    template_name = 'player/add_player.html'
+        data['Tittle'] = "Add Player"
+    template_name = 'player/add.html'
     return render(request, template_name, data)
 
-
-def detail(request, player_id):
-
+def add_coach(request):
     data = {}
-    template_name = 'player/detail_player.html'
+    if request.method == "POST":
+        data['form'] = CoachForm(request.POST, request.FILES)
 
-    # SELECT * FROM player WHERE id = player_id
-    data['player'] = Player.objects.get(pk=player_id)
-    # import pdb;pdb.set_trace()
+        if data['form'].is_valid():
+            # aca el formulario valido
+            data['form'].save()
+
+            return redirect('player_list')
+
+    else:
+        data['form'] = CoachForm()
+        data['Tittle'] = "Add Coach"
+    template_name = 'player/add.html'
+    return render(request, template_name, data)
+
+def add_team(request):
+    data = {}
+    if request.method == "POST":
+        data['form'] = TeamForm(request.POST, request.FILES)
+
+        if data['form'].is_valid():
+            # aca el formulario valido
+            data['form'].save()
+
+            return redirect('player_list')
+
+    else:
+        data['form'] = TeamForm()
+        data['Tittle'] = "Add Team"
+
+
+    template_name = 'player/add.html'
+    return render(request, template_name, data)
+
+def edit_player(request, player_id):
+    data = {}
+    if request.POST:
+        formPlayer = EditPlayer(request.POST, request.FILES, instance=Player.objects.get(pk=player_id))
+        if formPlayer.is_valid():
+            formPlayer.save()
+            return HttpResponseRedirect(reverse('player_list'))
+    template_name = 'player/edit.html'
+    data['dat'] = EditPlayer(instance=Player.objects.get(pk=player_id))
+    data['Tittle'] = "Edit Player"
+
 
     return render(request, template_name, data)
 
+def edit_team(request, team_id):
+    data = {}
+    if request.POST:
+        formPlayer = EditTeam(request.POST, request.FILES, instance=Team.objects.get(pk=team_id))
+        if formPlayer.is_valid():
+            formPlayer.save()
+            return HttpResponseRedirect(reverse('player_list'))
+    template_name = 'player/edit.html'
+    data['dat'] = EditTeam(instance=Team.objects.get(pk=team_id))
+    data['Tittle'] = "Edit Team"
 
+
+    return render(request, template_name, data)
+
+def edit_coach(request, coach_id):
+    data = {}
+    if request.POST:
+        formPlayer = EditCoach(request.POST, request.FILES, instance=Coach.objects.get(pk=coach_id))
+        if formPlayer.is_valid():
+            formPlayer.save()
+            return HttpResponseRedirect(reverse('player_list'))
+    template_name = 'player/edit.html'
+    data['dat'] = EditCoach(instance=Coach.objects.get(pk=coach_id))
+    data['Tittle'] = "Edit Coach"
+
+    return render(request, template_name, data)
+
+def Delete(request, id):
+    data = {}
+    template_name = 'listar.html'
+    data['player'] = Player.objects.all()
+    Player.objects.filter(pk=id).delete()
+    return HttpResponseRedirect(reverse('player_list'))
